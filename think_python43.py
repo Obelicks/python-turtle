7ce017945515b9dd83d31b6a67018aeb4e6dd31a
keg5su <ecb@rsyd.dk>
2023-08-30 11:28:57 +0200

    Add Polygon (Exercise 3 Solved)

--- think_python43.py
+++ think_python43.py
@@ -6,9 +6,15 @@ def square(t, length):
         t.lt(90)
         t.fd(length)
 
+def polygon(t, length,sides):
+    t.fd(length)
+    for i in range(sides-1):
+        t.lt(360/sides)
+        t.fd(length)
 
 bob = turtle.Turtle()
 #print(bob)
 
-square(bob, 1000)
+#square(bob, 1000)
+polygon(bob,100,20)
 turtle.mainloop()
